1d4029a9e16de3bf5efc3dd1f7b55be9f2e77a2e
Mike Garde <mgarde@fulfillment.com>
2016-03-11 11:36:31 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # periscopeToSQL
+
+periscopedata.com is a powerful and useful tool for generating reports, but sometimes I want to put what I write in Periscope to use elsewhere. This package allows you to write once but generate MySQL statements with filters and daterange within PHP.
+
+### Limits
+
+You bet...
+
+* UTC is assumed to be used by your DB
+* Always converts to EST
+
+###  Installation
+
+```
+composer require fulfillment/api-wrapper
+
+```
+
+### Version
+
+0.1
+
+### Contribute
+
+Feel free, time zone support may be a good place.
---
+++ src/periscopeToSQL.php
@@ -0,0 +1,73 @@
+<?php  namespace Fulfillment\periscopeToSQL;
+
+class periscopeToSQL {
+
+	function get($template, $fields) {
+
+		if (preg_match('/\[([a-z\_]+)\:est\]/i', $template, $matches)) {
+			$replacement = '(date_add(' . $matches[1] . ', interval -5 hour))';
+			$replace     = '[' . $matches[1] . ':est]';
+			$template    = str_replace($replace, $replacement, $template);
+		}
+
+		if (preg_match('/\[([a-z\_]+)\=daterange(:est)?\]/i', $template, $matches)) {
+
+			$start = abs(strtotime($fields['from_date']) + (60 * 60 * 5) - time());
+			$end   = abs(strtotime($fields['to_date']) + (60 * 60 * 5) - time());
+			$start = floor($start / (60 * 60 * 24));
+			$end   = floor($end / (60 * 60 * 24)) - 1; // minus 1 for end-of-day
+
+			$startSql = $endSql = 'DATE_ADD(DATE(now()), interval 5 hour)';
+
+			if ($start) {
+				$startSql = 'DATE_SUB(' . $startSql . ', interval ' . $start . ' day)';
+			}
+			if ($end) {
+				$endSql = 'DATE_SUB(' . $endSql . ', interval ' . $end . ' day)';
+			}
+
+			$replacement = $matches[1] . ' >= ' . $startSql . ' AND ' . $matches[1] . ' < ' . $endSql;
+
+			$replace  = [
+				'[' . $matches[1] . '=daterange:est]',
+				'[' . $matches[1] . '=daterange]',
+			];
+			$template = str_replace($replace, $replacement, $template);
+		}
+
+		foreach ($fields as $key => $value) {
+
+			if (preg_match('/\[(.*)=' . $key . '\]/', $template, $matches)) {
+
+				if (!$value) {
+					$replacement = '1=1';
+				} else {
+					$replacement = $matches[1];
+
+					if (isRepeating($value)) {
+						$replacement .= ' IN (' . $value . ')';
+					} else {
+						$replacement .= ' = ' . $value;
+					}
+				}
+				$replace  = '[' . $matches[1] . '=' . $key . ']';
+				$template = str_replace($replace, $replacement, $template);
+			}
+		}
+
+		return $template;
+	}
+
+	function isRepeating($value) {
+
+		if (preg_match('/^[0-9,]+,[0-9]+$/', $value)) {
+			return true;
+		}
+		if (preg_match('/^("(.*)",)*"(.*)"$/', $value)) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
